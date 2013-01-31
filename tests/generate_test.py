8bea6d99dfe6ffb93bba7e5cfa62565e165b2e41
okard <d4rkdragon@gmail.com>
2013-02-01 00:23:03 +0100

    working on tests

---
+++ tests/generate_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+import os.path
+
+
+f = open('CMakeLists_.txt', 'w')
+
+def write_test(f, fname):
+	print(fname)
+	name=fname[:-4]
+	f.write('add_executable({0}_exc {1})\n'.format(name, fname))
+	f.write('target_link_libraries({0}_exc culio culsys cullog)\n'.format(name))
+	f.write('add_test({0} {1}/{0}_exc)\n'.format(name, '${EXECUTABLE_OUTPUT_PATH}'))
+	f.write('\n')
+	
+for root, dirs, files in os.walk('.'):
+	#print(files)
+	for file in files:
+		if file.endswith('.cpp'):
+			write_test(f, file)
+				
+f.close();
+

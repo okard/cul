2c64dfbe3671968eed92cdee4d12e9413394e51f
okard <d4rkdragon@gmail.com>
2013-11-05 15:13:30 +0100

    add pimpl utility class

---
+++ include/culcore/DPtrImpl.hpp
@@ -0,0 +1,61 @@
+/*
+    C++ Utility Library
+
+    Copyright (c) 2013  okard
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
+#pragma once
+#ifndef CUL_DPTRIMPL_HPP
+#define CUL_DPTRIMPL_HPP
+
+namespace cul {
+	
+//Basic Constructor
+template <typename T>
+dptr<T>::dptr()
+	: obj(new T())
+{
+}
+ 
+//Forwarded Constructor
+template <typename T>
+template <typename ...Args>
+dptr<T>::dptr(Args&& ...args)
+	: obj(new T(std::forward<Args>(args)... ))
+{
+	
+}
+
+//Destructor
+template <typename T>
+dptr<T>::~dptr()
+{
+}
+
+//Access
+template <typename T>
+T* dptr<T>::operator->() const
+{
+	return obj.get();
+}
+    	
+} //end namespace cul
+
+#endif //CUL_DPTRIMPL_HPP
